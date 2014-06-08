681d8b8459ddf2d1470d3f6fea8c58d7931668b4
Li Dong <dongli@lasg.iap.ac.cn>
2014-06-08 18:10:26 +0800

    Update to NCL 6.2.0

--- create_ncl_completions.rb
+++ create_ncl_completions.rb
@@ -1,8 +1,8 @@
-#!/usr/bin/env ruby
+#!/usr/bin/env ruby -wU
 
 if File.exist?("NCL.sublime-completions")
-    print "[Warning]: NCL.sublime-completions exists, check it since the creation process may be long!\n"
-    exit
+    print "[Warning]: NCL.sublime-completions exists, delete it!\n"
+    File.delete("NCL.sublime-completions")
 end
 
 completion_file = File.new("NCL.sublime-completions", "w+")
@@ -11,45 +11,42 @@ completion_file.write("    \"scope\": \"source.ncl\",\n")
 completion_file.write("    \"completions\":\n")
 completion_file.write("    [\n")
 
+url_prefix = "http://www.ncl.ucar.edu"
+
 print "[Notice]: Grabing function definitions from NCL website.\n"
-page1 = `lynx -dump http://www.ncl.ucar.edu/Document/Functions/list_alpha.shtml`
-page1.encode!('UTF-8', 'UTF-8', :invalid => :replace, :replace => '')
+page1 = `curl -s #{url_prefix}/Document/Functions/list_alpha.shtml`
 
 categories = ['Functions/Built-in', 'Functions/Contributed', 'Functions/WRF_arw', 'Graphics/Interfaces']
 categories.each do |category|
-    func_urls = page1.scan(/http:\/\/www\.ncl\.ucar\.edu\/Document\/#{category}\/\w*\.shtml/)
-    func_urls.each do |func_url|
-        func = func_url.match(/(\w+)(\.shtml)/)[1]
-        page2 = `lynx -dump #{func_url}`
-        page2.encode!('UTF-8', 'UTF-8', :invalid => :replace, :replace => '')
-        puts "[Notice]: Creating completion for #{func}."
-        completion_file.write("        { \"trigger\": \"#{func}\", \"contents\": \"#{func}(")
-        prototype = page2.match(/(function|procedure) \w+ \(([^\)]*)\)$/m)[2].strip
-        i = 1
-        prototype.each_line do |line|
-            arg = line.split(" ")[0].strip
-            completion_file.write("${#{i}:#{arg}}")
-            completion_file.write(", ") if i != prototype.lines.count
-            i += 1
-        end
-        completion_file.write(")\" },\n")
-    end
+   func_fields = page1.scan(/<a href="\/Document\/#{category}\/\w*\.shtml/)
+   func_fields.each do |func_field|
+       func = func_field.match(/(\w+)(\.shtml)/)[1]
+       func_url = "#{url_prefix}#{func_field.match(/<a href="(.*)/)[1]}"
+       page2 = `curl -s #{func_url}`
+       puts "[Notice]: Creating completion for #{func}."
+       completion_file.write("        { \"trigger\": \"#{func}\", \"contents\": \"#{func}(")
+       prototype = page2.match(/(function|procedure) \w+ \(([^\)]*)\)$/m)[2].strip
+       i = 1
+       prototype.each_line do |line|
+           arg = line.split(" ")[0].strip
+           completion_file.write("${#{i}:#{arg}}")
+           completion_file.write(", ") if i != prototype.lines.count
+           i += 1
+       end
+       completion_file.write(")\" },\n")
+   end
 end
 
 print "[Notice]: Grabing graphics resources from NCL website.\n"
-page1 = `lynx -dump http://www.ncl.ucar.edu/Document/Graphics/Resources/list_alpha_res.shtml`
-page1.encode!('UTF-8', 'UTF-8', :invalid => :replace, :replace => '')
+page1 = `curl -s http://www.ncl.ucar.edu/Document/Graphics/Resources/list_alpha_res.shtml`
 
 resources = [] # There may be duplicate links in NCL graphics resources webpage.
-categories = ['Graphics/Resources']
-categories.each do |category|
-    res_urls = page1.scan(/http:\/\/www\.ncl\.ucar\.edu\/Document\/#{category}\/\w+\.shtml#[a-z]\w+$/)
-    res_urls.each do |res_url|
-        res = res_url.match(/(\w+)$/)[1]
-        if not resources.include?(res)
-            completion_file.write("        \"#{res}\",\n")
-            resources.push(res)
-        end
+res_fields = page1.scan(/^<a name="\w+/)
+res_fields.each do |res_field|
+    res = res_field.match(/(\w+)$/)[1]
+    if not resources.include?(res)
+        completion_file.write("        \"#{res}\",\n")
+        resources.push(res)
     end
 end
 
