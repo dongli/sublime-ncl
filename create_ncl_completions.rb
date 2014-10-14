83745120e8591c7ecf682fd3a499ac1881ce59ce
Li Dong <dongli@lasg.iap.ac.cn>
2014-10-14 13:19:20 +0800

    Fix Ruby scripts and update to 6.2.1

--- create_ncl_completions.rb
+++ create_ncl_completions.rb
@@ -1,53 +1,79 @@
 #!/usr/bin/env ruby -wU
 
-if File.exist?("NCL.sublime-completions")
-    print "[Warning]: NCL.sublime-completions exists, delete it!\n"
-    File.delete("NCL.sublime-completions")
+if File.exist? 'NCL.sublime-completions'
+  print "[Warning]: NCL.sublime-completions exists, delete it!\n"
+  File.delete 'NCL.sublime-completions'
 end
 
-completion_file = File.new("NCL.sublime-completions", "w+")
-completion_file.write("{\n")
-completion_file.write("    \"scope\": \"source.ncl\",\n")
-completion_file.write("    \"completions\":\n")
-completion_file.write("    [\n")
+completion_file = File.new 'NCL.sublime-completions', 'w+'
+completion_file << "{\n"
+completion_file << "    \"scope\": \"source.ncl\",\n"
+completion_file << "    \"completions\":\n"
+completion_file << "    [\n"
 
-url_prefix = "http://www.ncl.ucar.edu"
+url_prefix = 'http://www.ncl.ucar.edu'
 
 print "[Notice]: Grabing function definitions from NCL website.\n"
 page1 = `curl -s #{url_prefix}/Document/Functions/list_alpha.shtml`
 
-categories = ['Functions/Built-in', 'Functions/Contributed', 'Functions/WRF_arw', 'Graphics/Interfaces']
+categories = ['Functions/Built-in', 'Functions/Contributed', 'Functions/User_contributed', 'Functions/WRF_arw', 'Graphics/Interfaces']
 categories.each do |category|
-   func_fields = page1.scan(/<a href="\/Document\/#{category}\/\w*\.shtml/)
-   func_fields.each do |func_field|
-       func = func_field.match(/(\w+)(\.shtml)/)[1]
-       func_url = "#{url_prefix}#{func_field.match(/<a href="(.*)/)[1]}"
-       page2 = `curl -s #{func_url}`
-       puts "[Notice]: Creating completion for #{func}."
-       completion_file.write("        { \"trigger\": \"#{func}\", \"contents\": \"#{func}(")
-       prototype = page2.match(/(function|procedure) \w+ \(([^\)]*)\)$/m)[2].strip
-       i = 1
-       prototype.each_line do |line|
-           arg = line.split(" ")[0].strip
-           completion_file.write("${#{i}:#{arg}}")
-           completion_file.write(", ") if i != prototype.lines.count
-           i += 1
-       end
-       completion_file.write(")\" },\n")
-   end
+  page1.scan(/^\s*<a href="\/Document\/#{category}\/\w*\.shtml/).each do |x|
+    func = x.match(/(\w+)(\.shtml)/)[1]
+    func_url = "#{url_prefix}#{x.match(/<a href="(.*)/)[1]}"
+    page2 = `curl -s #{func_url}`
+    puts "[Notice]: Creating completion for #{func}."
+    completion_file << "        { \"trigger\": \"#{func}\", \"contents\": \"#{func}("
+    begin
+      prototype = page2.match(/(function|procedure) \w+ \(([^\)]*)\)$/m)[2].strip
+    rescue
+      print page2.match(/(function|procedure) \w+ \(([^\)]*)\)$/m)
+      print "[Error]: Failed to extract prototype for #{func}!"
+      exit
+    end
+    i = 1
+    prototype.each_line do |line|
+      arg = line.split(" ")[0].strip
+      completion_file << "${#{i}:#{arg}}"
+      completion_file << ", " if i != prototype.lines.count
+      i += 1
+    end
+    completion_file << ")\" },\n"
+  end
 end
 
 print "[Notice]: Grabing graphics resources from NCL website.\n"
-page1 = `curl -s http://www.ncl.ucar.edu/Document/Graphics/Resources/list_alpha_res.shtml`
+page1 = `curl -s #{url_prefix}/Document/Graphics/Resources/list_alpha_res.shtml`
 
 resources = [] # There may be duplicate links in NCL graphics resources webpage.
-res_fields = page1.scan(/^<a name="\w+/)
-res_fields.each do |res_field|
-    res = res_field.match(/(\w+)$/)[1]
-    if not resources.include?(res)
-        completion_file.write("        \"#{res}\",\n")
-        resources.push(res)
-    end
+page1.scan(/^<a name="\w+"><\/a><strong>/).each do |x|
+  res = x.match(/"(\w+)"></)[1]
+  if not resources.include? res
+    completion_file << "        \"#{res}\",\n"
+    resources << res
+  end
+end
+
+print "[Notice]: Grabing resource codes from NCL website.\n"
+codes = []
+page1.scan(/<code>\w+<\/code>/).each do |x|
+  code = x.match(/>(\w+)</)[1]
+  if not codes.include? code
+    completion_file << "        \"#{code}\",\n"
+    codes << code
+  end
+end
+
+print "[Notice]: Grabing color tables from NCL website.\n"
+page1 = `curl -s #{url_prefix}/Document/Graphics/color_table_gallery.shtml`
+
+color_tables = []
+page1.scan(/^<td>\w+<br>$/).each do |x|
+  color_table = x.match(/>(\w+)</)[1]
+  if not color_tables.include? color_table
+    completion_file << "        \"#{color_table}\",\n"
+    color_tables << color_table
+  end
 end
 
 completion_file.write("    ]\n")
--- create_ncl_syntax.rb
+++ create_ncl_syntax.rb
@@ -3,153 +3,143 @@
 require 'nokogiri'
 
 builder = Nokogiri::XML::Builder.new(:encoding => 'UTF-8') do |xml|
-xml.doc.create_internal_subset(
+  xml.doc.create_internal_subset(
     'plist',
     "-//Apple//DTD PLIST 1.0//EN",
     "http://www.apple.com/DTDs/PropertyList-1.0.dtd"
-)
-xml.plist(:version => '1.0') {
+  )
+  xml.plist(:version => '1.0') {
     xml.dict {
-        xml.key "fileTypes"
-        xml.array {
-            xml.string "ncl"
+      xml.key "fileTypes"
+      xml.array {
+        xml.string "ncl"
+      }
+      xml.key "name"
+      xml.string "NCL"
+      xml.key "scopeName"
+      xml.string "source.ncl"
+      xml.key "uuid"
+      xml.string "43d57527-c0c4-46f3-a175-08a320bc52de"
+      xml.key "patterns"
+      xml.array {
+        xml.dict {
+          xml.key "name"
+          xml.string "comment.line.source.ncl"
+          xml.key "comment"
+          xml.string "NCL comment"
+          xml.key "match"
+          xml.string ";.*$"
         }
-        xml.key "name"
-        xml.string "NCL"
-        xml.key "scopeName"
-        xml.string "source.ncl"
-        xml.key "uuid"
-        xml.string "43d57527-c0c4-46f3-a175-08a320bc52de"
-        xml.key "patterns"
-        xml.array {
-            # ------------------------------------------------------------------
-            xml.dict {
-                xml.key "name"
-                xml.string "comment.line.source.ncl"
-                xml.key "comment"
-                xml.string "NCL comment"
-                xml.key "match"
-                xml.string ";.*$"
-            }
-            # ------------------------------------------------------------------
-            xml.dict {
-                xml.key "name"
-                xml.string "support.type.source.ncl"
-                xml.key "comment"
-                xml.string "NCL types"
-                xml.key "match"
-                xml.string "\\b(integer|float|double|string|graphic)\\b"
-            }
-            # ------------------------------------------------------------------
-            xml.dict {
-                xml.key "name"
-                xml.string "constant.numeric.source.ncl"
-                xml.key "comment"
-                xml.string "NCL numerics"
-                xml.key "match"
-                xml.string "\\b(\\+|-)?\\d+(\\.\\d+((d|D|e|E)(\\+|-)?\\d+)?)?\\b"
-            }
-            # ------------------------------------------------------------------
-            xml.dict {
-                xml.key "name"
-                xml.string "keyword.operator.source.ncl"
-                xml.key "comment"
-                xml.string "NCL operators"
-                xml.key "match"
-                xml.string "\\.(eq|ne|gt|ge|lt|le|not|and|or)\\."
-            }
-            # ------------------------------------------------------------------
-            xml.dict {
-                xml.key "name"
-                xml.string "keyword.control.source.ncl"
-                xml.key "comment"
-                xml.string "NCL control keywords"
-                xml.key "match"
-                xml.string "\\b(do|end|if|then|else|while|break|continue|return|load|begin|end|procedure|function|local)\\b"
-            }
-            # ------------------------------------------------------------------
-            xml.dict {
-                xml.key "name"
-                xml.string "constant.language.source.ncl"
-                xml.key "comment"
-                xml.string "NCL boolean"
-                xml.key "match"
-                xml.string "\\b(True|False)\\b"
-            }
-            # ------------------------------------------------------------------
-            xml.dict {
-                xml.key "name"
-                xml.string "string.quoted.double.source.ncl"
-                xml.key "comment"
-                xml.string "NCL string"
-                xml.key "begin"
-                xml.string "\""
-                xml.key "end"
-                xml.string "\""
-                xml.key "patterns"
-                xml.array {
-                    xml.dict {
-                        xml.key "name"
-                        xml.string "constant.character.escape.source.ncl"
-                        xml.key "match"
-                        xml.string "\\."
-                    }
-                    xml.dict {
-                        xml.key "name"
-                        xml.string "storage.type.source.ncl"
-                        xml.key "comment"
-                        xml.string "Environment variable"
-                        xml.key "match"
-                        xml.string "\\$\\w+"
-                    }
-                }
-            }
-            # ------------------------------------------------------------------
-            xml.dict {
-                xml.key "name"
-                xml.string "support.function.source.ncl"
-                xml.key "comment"
-                xml.string "NCL intrinsic functions"
-                print "[Notice]: Grabing functions from NCL webpage.\n"
-                page1 = `curl -s http://www.ncl.ucar.edu/Document/Functions/list_alpha.shtml`
-                categories = ['Functions/Built-in', 'Functions/Contributed', 'Functions/WRF_arw', 'Graphics/Interfaces']
-                string = ""
-                categories.each do |category|
-                   func_fields = page1.scan(/<a href="\/Document\/#{category}\/\w*\.shtml/)
-                   func_fields.each do |func_field|
-                       func = func_field.match(/(\w+)(\.shtml)/)[1]
-                       string += "#{func}|"
-                   end
-                end
-                xml.key "match"
-                xml.string "\\b(#{string[0..-2]})\\b"
-            }
-            # ------------------------------------------------------------------
-            xml.dict {
-                xml.key "name"
-                xml.string "support.other.source.ncl"
-                xml.key "comment"
-                xml.string "Resources"
-                print "[Notice]: Grabing resources from NCL website.\n"
-                page1 = `curl -s http://www.ncl.ucar.edu/Document/Graphics/Resources/list_alpha_res.shtml`
-                resources = [] # There may be duplicate links in NCL graphics resources webpage.
-                string = ""
-                res_fields = page1.scan(/^<a name="\w+/)
-                res_fields.each do |res_field|
-                    res = res_field.match(/(\w+)$/)[1]
-                    if not resources.include?(res)
-                        string += "#{res}|"
-                        resources.push(res)
-                    end
-                end
-                xml.key "match"
-                xml.string "\\b(#{string[0..-2]})\\b"
-            }
+        xml.dict {
+          xml.key "name"
+          xml.string "support.type.source.ncl"
+          xml.key "comment"
+          xml.string "NCL types"
+          xml.key "match"
+          xml.string "\\b(integer|float|double|string|graphic)\\b"
         }
+        xml.dict {
+          xml.key "name"
+          xml.string "constant.numeric.source.ncl"
+          xml.key "comment"
+          xml.string "NCL numerics"
+          xml.key "match"
+          xml.string "\\b(\\+|-)?\\d+(\\.\\d+((d|D|e|E)(\\+|-)?\\d+)?)?\\b"
+        }
+        xml.dict {
+          xml.key "name"
+          xml.string "keyword.operator.source.ncl"
+          xml.key "comment"
+          xml.string "NCL operators"
+          xml.key "match"
+          xml.string "\\.(eq|ne|gt|ge|lt|le|not|and|or)\\."
+        }
+        xml.dict {
+          xml.key "name"
+          xml.string "keyword.control.source.ncl"
+          xml.key "comment"
+          xml.string "NCL control keywords"
+          xml.key "match"
+          xml.string "\\b(do|end|if|then|else|while|break|continue|return|load|begin|end|procedure|function|local)\\b"
+        }
+        xml.dict {
+          xml.key "name"
+          xml.string "constant.language.source.ncl"
+          xml.key "comment"
+          xml.string "NCL boolean"
+          xml.key "match"
+          xml.string "\\b(True|False)\\b"
+        }
+        xml.dict {
+          xml.key "name"
+          xml.string "string.quoted.double.source.ncl"
+          xml.key "comment"
+          xml.string "NCL string"
+          xml.key "begin"
+          xml.string "\""
+          xml.key "end"
+          xml.string "\""
+          xml.key "patterns"
+          xml.array {
+            xml.dict {
+              xml.key "name"
+              xml.string "constant.character.escape.source.ncl"
+              xml.key "match"
+              xml.string "\\."
+            }
+            xml.dict {
+              xml.key "name"
+              xml.string "storage.type.source.ncl"
+              xml.key "comment"
+              xml.string "Environment variable"
+              xml.key "match"
+              xml.string "\\$\\w+"
+            }
+          }
+        }
+        xml.dict {
+          xml.key "name"
+          xml.string "support.function.source.ncl"
+          xml.key "comment"
+          xml.string "NCL intrinsic functions"
+          print "[Notice]: Grabing functions from NCL webpage.\n"
+          page = `curl -s http://www.ncl.ucar.edu/Document/Functions/list_alpha.shtml`
+          categories = ['Functions/Built-in', 'Functions/Contributed',
+                        'Functions/User_contributed', 'Functions/WRF_arw', 'Graphics/Interfaces']
+          string = ''
+          categories.each do |category|
+            page.scan(/^\s*<a href="\/Document\/#{category}\/\w*\.shtml/).each do |x|
+              func = x.match(/(\w+)(\.shtml)/)[1]
+              string += "#{func}|"
+            end
+          end
+          xml.key "match"
+          xml.string "\\b(#{string[0..-2]})\\b"
+        }
+        xml.dict {
+          xml.key "name"
+          xml.string "support.other.source.ncl"
+          xml.key "comment"
+          xml.string "Resources"
+          print "[Notice]: Grabing resources from NCL website.\n"
+          page = `curl -s http://www.ncl.ucar.edu/Document/Graphics/Resources/list_alpha_res.shtml`
+          resources = [] # There may be duplicate links in NCL graphics resources webpage.
+          string = ''
+          page.scan(/^<a name="\w+"><\/a><strong>/).each do |x|
+            res = x.match(/"(\w+)"></)[1]
+            if not resources.include? res
+              string << "#{res}|"
+              resources << res
+            end
+          end
+          xml.key 'match'
+          xml.string "\\b(#{string[0..-2]})\\b"
+        }
+      }
     }
-}
+  }
 end
 
-file = File.open("NCL.tmLanguage", "w")
-file.write builder.to_xml
-file.close
+File.open('NCL.tmLanguage', 'w') do |file|
+  file << builder.to_xml
+end
